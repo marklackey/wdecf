01f1812d196e9c0fa873232fa4cb76e2babdc526
bearmountain <superfun@gmail.com>
2021-10-16 18:58:37 -0700

    tweaky tweaks

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let maxSpeed;
 let fps;
 const ABSOLUTE_MAX = 500;
-const eelText = "Where do eels come from? ";
+const eelText = "Where do eels come from?";
 let index = 0;
 let hue = 0;
 let spanId = 0;
@@ -9,27 +9,28 @@ let spanId = 0;
 function addNextLetter(index) {
     if (index == eelText.length) {
         const choice = getRandomInt(15);
-        if (choice == 14) {
+        if (choice > 13) {
             index = 0;
-            document.getElementById(spanId).innerHTML += "<br>";
+            document.getElementById("eels").innerHTML += "<br>";
 
+        } else if (choice > 10) {
+            index--;
         } else {
             index = 0;
         }
     }
     if (index == 0) {
         maxSpeed = getRandomInt(ABSOLUTE_MAX);
-        spanId++;
 
         const newSpan = document.createElement('span');
-        newSpan.id = `${spanId}`;
         newSpan.style.marginLeft = `${getRandomInt(10)}rem`;
         newSpan.style.color = `hsla(${hue},100%, ${getRandomInt(50)+50}%, 0.8)`;
+        newSpan.style.fontSize = `${Math.random() * 2 +1}rem`;
 
         document.getElementById("eels").append(newSpan);
 
     }
-    document.getElementById(spanId).innerHTML = `${document.getElementById(spanId).innerHTML}${eelText[index]}`;
+    document.getElementById("eels").lastChild.innerHTML += eelText[index];
     index++;
     hue += getRandomInt(50);
     fps = getRandomInt(maxSpeed);
